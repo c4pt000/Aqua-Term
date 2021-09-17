87c4ed7a2fd3cef826ce83ea4d24a7e0c9f82eff
c4pt000 <46433702+c4pt000@users.noreply.github.com>
2021-09-17 00:56:36 -0400

    Update create-keystore-and-sign.sh

--- create-keystore-and-sign.sh
+++ create-keystore-and-sign.sh
@@ -5,6 +5,8 @@ npm -g install randomstring'
 randomstring
 rm -rf my-release-key.jks
 keytool -genkey -v -keystore my-release-key.jks -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
-cp -rf wallet/build/outputs/apk/prod/release/wallet-prod-release-unsigned.apk .
-/home/Android/Sdk/build-tools/29.0.3/apksigner sign --ks my-release-key.jks --out RadioCoin-Android-signed.apk wallet-prod-release-unsigned.apk
+
+# to copy unsigned apk here to this directory rename wallet-prod-release-unsigned.apk to "apk-to-sign.apk" with  --out Android-signed.apk as signed apk name
+#cp -rf wallet/build/outputs/apk/prod/release/wallet-prod-release-unsigned.apk .
+/home/Android/Sdk/build-tools/29.0.3/apksigner sign --ks my-release-key.jks --out Android-signed.apk wallet-prod-release-unsigned.apk
  
